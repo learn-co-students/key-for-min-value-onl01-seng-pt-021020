652637c6292853b0e1c3dde96d1054751b695c56
excellent-revision-4527 <tjcroley@outlook.com>
2020-02-22 22:08:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  name_hash = {"Victor" => 18, "Dimitry" => 42, "Roman" => 12, "Oleks" => 27}
+
 
 end
